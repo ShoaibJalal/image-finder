aa26a60155d9d2dfd68b0df99cc42bcc8f490414
shoaibjalal <shoaib.jalal@myy.haaga-helia.fi>
2019-07-18 18:25:26 +0300

    Updates

---
+++ precache-manifest.5fa93679f3bbbcb472a425181a55f8af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8e10000fc988121e325",
+    "url": "/image-finder/static/js/runtime~main.f8e10000.js"
+  },
+  {
+    "revision": "ee7034e649ab31adf582",
+    "url": "/image-finder/static/js/main.ee7034e6.chunk.js"
+  },
+  {
+    "revision": "6b428ded1a106aed8295",
+    "url": "/image-finder/static/js/1.6b428ded.chunk.js"
+  },
+  {
+    "revision": "ee7034e649ab31adf582",
+    "url": "/image-finder/static/css/main.02d00fc2.chunk.css"
+  },
+  {
+    "revision": "95b554f0892114ff94c3295fee6daf39",
+    "url": "/image-finder/index.html"
+  }
+];
